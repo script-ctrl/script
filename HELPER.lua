43be3a59002ec1065654e467a51ac85941d0a07c
script-ctrl <tamerlan.alizade062@gmail.com>
2025-06-29 23:16:38 +0400

    Create HELPER.lua

---
+++ HELPER.lua
@@ -0,0 +1,4 @@
+pcall(function()
+    sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius", math.huge)
+    sethiddenproperty(game.Players.LocalPlayer, "MaximumSimulationRadius", math.huge)
+end)
